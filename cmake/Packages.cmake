ef2c496b9c92d59212fe852116680a1d7e074255
James Orson <jamesaorson@gmail.com>
2020-04-25 04:31:39 -0700

    KCPP-3: Link SFML and create hello world

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -1,2 +1,13 @@
 # spdlog
 add_subdirectory("packages/spdlog")
+
+# sfml
+set(SFML_STATIC_LIBRARIES TRUE)
+SET(CMAKE_FIND_FRAMEWORK LAST)
+set(BUILD_SHARED_LIBS FALSE)
+add_subdirectory("packages/SFML")
+
+# set(SFML_DIR "packages/SFML/build")
+# set(CMAKE_MODULE_PATH "../packages/SFML/build")
+# set(SFML_STATIC_LIBRARIES TRUE)
+# find_package(SFML 2 COMPONENTS audio network graphics window system)
--- setup.sh
+++ setup.sh
@@ -5,8 +5,35 @@ cd packages
 echo "Setting up spdlog..."
 cd spdlog
 
-mkdir -f build
+echo "    Making build directory"
+mkdir -p build
+echo "    Moving to build directory"
 cd build
+echo "    Running cmake"
 cmake ..
+echo "    Running make"
 make -j
+echo "    Running make install"
+echo ""
 make install
+
+# Reset directory
+cd ../..
+
+# Set up SFML
+echo "Setting up SFML..."
+cd SFML
+
+echo "    Making build directory"
+mkdir -p build
+echo "    Moving to build directory"
+cd build
+echo "    Running cmake"
+cmake ..
+echo "    Running make"
+make -j
+echo "    Running make install"
+echo ""
+make install
+
+echo "Setup done..."
